a507bfb909f9bd2d811c63a1f8fc9b0374038609
octavdragoi <octavdragoi@alumni.harvard.edu>
2019-11-30 12:05:31 +0100

    changed path of htmls

--- WebDBSite/settings.py
+++ WebDBSite/settings.py
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'WebDBSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'assets/src/html/')],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
