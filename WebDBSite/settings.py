a433bb7a544091f80a23f3029a8cd79bf4e01c00
MMHetman <57961266+mmhetman@users.noreply.github.com>
2019-12-02 14:35:41 +0100

    Windows configuration

--- WebDBSite/settings.py
+++ WebDBSite/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
 # Configure webpack loader
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'dist',
+        'BUNDLE_DIR_NAME': 'dist/',
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
