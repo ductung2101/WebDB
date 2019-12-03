4ecad09b1d8398c9d4bbe648bc03c565bd1513df
octavdragoi <octavdragoi@alumni.harvard.edu>
2019-12-03 22:16:47 +0100

    fixed url redirect

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main_page, name='index'),
     path('sub', views.sub, name='sub'),
     path('line_chart', views.line_chart, name='line_chart'),
     path('line_chart_json', views.line_chart_json, name='line_chart_json'),
