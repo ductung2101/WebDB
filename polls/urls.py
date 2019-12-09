51c3fae456535277a3d3925833e48cf88dccb91f
MMHetman <57961266+mmhetman@users.noreply.github.com>
2019-12-09 15:38:09 +0100

    Correlation matrix based on csv file - simple html

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.urls import path
 
 from . import views
@@ -8,5 +9,7 @@ urlpatterns = [
     path('line_chart', views.line_chart, name='line_chart'),
     path('line_chart_json', views.line_chart_json, name='line_chart_json'),
     path('poll_json', views.poll_json, name='poll_json'),
+    path('correlation_matrix', views.corMatix, name='correlation_matrix'),
+    # path('influence_json', views.influence_json, name='influence_json'),
     path('main_page', views.main_page, name='main_page'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,20 +2,43 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
+from chartjs.views.lines import HighchartPlotLineChartView
 from random import randint
 from polls.models import Poll
 import datetime
 import numpy as np
 import pandas as pd
 
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
 
 def sub(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+
+def corMatix(request):
+    df = pd.read_csv("polls\\simple_cor_data.csv")
+    df["polling_result"] = df["polling_result"].astype(float)
+    df["coverage"] = df["coverage"].astype(float)
+    colnames = df["candidate"].unique()
+    rownames = df["series"].unique()
+    cor_mat = pd.DataFrame(columns=colnames, index=rownames)
+    for col in colnames:
+        for row in rownames:
+            subset = df[df["candidate"] == col]
+            subset = subset[subset["series"] == row]
+
+            cor_mat.at[row, col] = np.corrcoef(subset["coverage"], subset["polling_result"])[0, 1]
+            if cor_mat.at[row, col] != cor_mat.at[row, col] or cor_mat.at[row, col] == 0 or cor_mat.at[
+                row, col] == -1 or cor_mat.at[row, col] == 1:
+                cor_mat.at[row, col] = "-"
+    return HttpResponse(cor_mat.to_html())
+
 
 class LineChartJSONView(BaseLineChartView):
     def get_labels(self):
@@ -33,22 +56,23 @@ class LineChartJSONView(BaseLineChartView):
                 [41, 92, 18, 3, 73, 87, 92],
                 [87, 21, 94, 0, 0, 13, 65]]
 
+
 class PollJSONView(BaseLineChartView):
-    def __init__(self, n_weeks = 10):
+    def __init__(self, n_weeks=10):
         qs = Poll.pdobjects.all()  # Use the Pandas Manager
         self.df = qs.to_dataframe()
         self.df["pct"] = self.df["pct"].astype(float)
-        self.df["create_date"] = pd.to_datetime(self.df["created_at"], 
-            infer_datetime_format = True)
-        self.df["create_week"] = (self.df['create_date'] - pd.to_timedelta(\
-            self.df['create_date'].dt.dayofweek, unit='d') + np.timedelta64(7, 'D'))\
+        self.df["create_date"] = pd.to_datetime(self.df["created_at"],
+                                                infer_datetime_format=True)
+        self.df["create_week"] = (self.df['create_date'] - pd.to_timedelta( \
+            self.df['create_date'].dt.dayofweek, unit='d') + np.timedelta64(7, 'D')) \
             .dt.normalize()
-        self.n_weeks = n_weeks 
+        self.n_weeks = n_weeks
         # TODO: insert better filtering here
         self.df_subset = self.df[self.df["party"] == "DEM"]
         self.df_pivot = self.df_subset.pivot_table(
-            values = "pct", index = "create_week", columns="candidate_name",
-            aggfunc = np.mean).fillna(0).iloc[-self.n_weeks:]
+            values="pct", index="create_week", columns="candidate_name",
+            aggfunc=np.mean).fillna(0).iloc[-self.n_weeks:]
         # import pdb; pdb.set_trace()
 
     def get_labels(self):
@@ -73,8 +97,25 @@ class PollJSONView(BaseLineChartView):
             lst_selected.append(list(self.df_pivot[can]))
         return lst_selected
 
+
+'''class MediaInfulenceJSONView(HighchartPlotLineChartView):
+    def __init__(self):
+        self.df = pd.read_csv('C:\\Users\\User\\Documents\\WebDB_data_analysis\\simple_cor_data.csv')
+        self.df["polling_result"] = self.df["polling_result"].astype(float)
+        self.df["coverage"] = self.df["coverage"].astype(float)
+
+    def get_series(self):
+        subset = self.df[self.df["candidate"] == "Trump"]
+        subset = subset[self.df["series"] == "CNN"]
+        series = [[subset['coverage']]]
+        return series
+
+    def get_labels(self):
+        return []'''
+
 main_page = TemplateView.as_view(template_name='index.html')
 line_chart = TemplateView.as_view(template_name='line_chart.html')
 
 line_chart_json = LineChartJSONView.as_view()
 poll_json = PollJSONView.as_view()
+# influence_json = MediaInfulenceJSONView.as_view()
