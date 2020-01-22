65cb07b81af9fa92bd24de6ea98846f5f93d1b72
Octav Dragoi <octav.dragoi@alumni.harvard.edu>
2020-01-22 20:38:59 +0100

    trying to format the form

--- polls/forms.py
+++ polls/forms.py
@@ -10,13 +10,18 @@ CANDIDATES=(
 
 class DateForm(forms.Form):
     daterange = forms.CharField(label='Date Range', initial='01.04.2019 - 01.08.2019')
-    candidates = forms.URLField(label = "Candidates", 
-        widget = BootstrapSelect(
-            attrs={
-                'class' : 'selectpicker',
-                'multiple' : ''},
-            choices = CANDIDATES),
+    candidates = forms.MultipleChoiceField(
+        choices=CANDIDATES,
+        widget = forms.CheckboxSelectMultiple(),
         required = False)
+        # widget=forms.CheckboxSelectMultiple()
+    # candidates = forms.CharField(label = "Candidates", 
+    #     widget = BootstrapSelect(
+    #         attrs={
+    #             'class' : 'selectpicker',
+    #             'multiple' : ''},
+    #         choices = CANDIDATES),
+    #     required = False)
 
 #if DateForm().is_valid():
 #    DateForm().save()
