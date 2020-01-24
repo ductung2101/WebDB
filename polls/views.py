b2a9dbbe7b45f900c2b9cceb49eec8b426f92c2e
MMHetman <57961266+mmhetman@users.noreply.github.com>
2020-01-24 15:11:47 +0100

    scatter plot and radar added

--- polls/views.py
+++ polls/views.py
@@ -13,6 +13,7 @@ import pytz
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 import os
 from plotly.offline import plot
 from django import forms
@@ -26,6 +27,7 @@ def parse_daterange(daterange):
     start_date = dates[0]
     end_date = dates[1]
     return start_date, end_date
+
 
 # Create your views here.
 def index(request):
@@ -43,24 +45,24 @@ class CorrelationView(View):
         form = DateForm(request.POST)
         if form.is_valid():
             print('yes done')
-        return redirect('correlation_view', start_date = form['start_date'].value(), end_date = form['end_date'].value())
+        return redirect('correlation_view', start_date=form['start_date'].value(), end_date=form['end_date'].value())
 
     @classmethod
-    def make_table(cls, start_date = None, end_date = None):
-        #try to replace df with model.Media
+    def make_table(cls, start_date=None, end_date=None):
+        # try to replace df with model.Media
         qs = Media.pdobjects.all()
         df = qs.to_dataframe()
 
         print("qs", qs[0])
         if start_date is not None and end_date is not None:
-            #change type of start_date to be same type with Media.date
+            # change type of start_date to be same type with Media.date
             start_date_new = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
             end_date_new = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
             mask = (df['date'] > start_date_new) & (df['date'] <= end_date_new)
             df = df.loc[mask]
         df["pct"] = df["pct"].astype(float)
         df["value"] = df["value"].astype(float)
-        #import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         colnames = df["candidate"].unique()
         rownames = df["series"].unique()
         cor_mat = pd.DataFrame(columns=colnames, index=rownames)
@@ -73,10 +75,10 @@ class CorrelationView(View):
                 if cor_mat.at[row, col] != cor_mat.at[row, col] or cor_mat.at[row, col] == 0 or cor_mat.at[
                     row, col] == -1 or cor_mat.at[row, col] == 1:
                     cor_mat.at[row, col] = "-"
-        
+
         return cor_mat.to_html()
 
-    def get(self, request, start_date = None, end_date = None):
+    def get(self, request, start_date=None, end_date=None):
         cor_mat = self.make_table(start_date, end_date)
         return HttpResponse(cor_mat)
 
@@ -85,7 +87,7 @@ class PollJSONView(BaseLineChartView):
     def post(self, request, *args, **kwargs):
         form = DateForm(request.POST)
         start_date, end_date = parse_daterange(form["daterange"].value())
-        return redirect('poll_json', start_date = start_date, end_date = end_date)
+        return redirect('poll_json', start_date=start_date, end_date=end_date)
 
     def do_compute(self):
         self.n_weeks = self.kwargs.get('n_weeks')
@@ -106,9 +108,9 @@ class PollJSONView(BaseLineChartView):
         self.df_subset = self.df[self.df["party"] == "DEM"]
         self.df_pivot = self.df_subset.pivot_table(
             values="pct", index="create_week", columns="candidate_name",
-            aggfunc=np.mean).fillna(0)#.iloc[-self.n_weeks:]
+            aggfunc=np.mean).fillna(0)  # .iloc[-self.n_weeks:]
         self.df_pivot = self.df_pivot[
-            (self.df_pivot.index <= self.end_date) & 
+            (self.df_pivot.index <= self.end_date) &
             (self.df_pivot.index >= self.start_date)]
         # import pdb; pdb.set_trace()
 
@@ -139,15 +141,15 @@ class PollJSONView(BaseLineChartView):
 class GDeltHeatmapView(View):
     def post(self, request, *args, **kwargs):
         form = DateForm(request.POST)
-        return redirect('gdelt_heatmap_view', start_date = form['start_date'].value(), 
-                                            end_date = form['end_date'].value())
+        return redirect('gdelt_heatmap_view', start_date=form['start_date'].value(),
+                        end_date=form['end_date'].value())
 
-    def get(self, request, start_date = None, end_date = None):
+    def get(self, request, start_date=None, end_date=None):
         plot_div = self.make_plot(start_date, end_date)
         return HttpResponse(plot_div)
 
     @classmethod
-    def make_plot(cls, start_date = None, end_date = None):
+    def make_heatmap(cls, start_date=None, end_date=None):
         df = pd.read_csv(os.path.join("polls", "corr_data.csv"))
         df['Date'] = pd.to_datetime(df['Date'])
         if start_date is not None and end_date is not None:
@@ -180,7 +182,60 @@ class GDeltHeatmapView(View):
 
         plot_div = plot(fig, output_type='div', include_plotlyjs=True)
         return plot_div
-    
+
+    @classmethod
+    def make_radar_chart(cls):
+        df = pd.read_csv(os.path.join("polls", "corr_data.csv"))
+        df['Date'] = pd.to_datetime(df['Date'])
+        start_date = datetime.datetime.strptime('2018-12-01', '%Y-%m-%d')
+        end_date = datetime.datetime.strptime('2018-12-30', '%Y-%m-%d')
+        mask = (df['Date'] > start_date) & (df['Date'] <= end_date)
+        df = df.loc[mask]
+
+        series_a = df['Series'] == 'MSNBC'
+        series_b = df['Series'] == 'CNN'
+        categories = df[series_a]['Candidate'][:5]
+        chart_data_a = df[series_a].groupby(['Candidate'])['Value'].agg('mean')[:5]
+        chart_data_b = df[series_b].groupby(['Candidate'])['Value'].agg('mean')[:5]
+        fig = go.Figure()
+
+        fig.add_trace(go.Scatterpolar(
+            r=chart_data_a,
+            theta=categories,
+            fill='toself',
+            name='Product A'
+        ))
+        fig.add_trace(go.Scatterpolar(
+            r=chart_data_b,
+            theta=categories,
+            fill='toself',
+            name='Product B'
+        ))
+
+        fig.update_layout(
+            polar=dict(
+                radialaxis=dict(
+                    visible=True,
+                    range=[0, 1]
+                )),
+            showlegend=False
+        )
+
+        plot_div = plot(fig, output_type='div', include_plotlyjs=True)
+        return plot_div
+
+    @classmethod
+    def make_scatter_plot(cls):
+        df = pd.read_csv(os.path.join("polls", "corr_data.csv"))
+        df['Date'] = pd.to_datetime(df['Date'])
+
+        df = df[df.Candidate == 'Warren']
+        df = df[df.Series == 'FOXNEWS']
+        fig = px.scatter(df, x="Value", y="pct", trendline="ols")
+
+        plot_div = plot(fig, output_type='div', include_plotlyjs=True)
+        return plot_div
+
 
 class MainPageView(FormView):
     template_name = 'index.html'
@@ -188,15 +243,20 @@ class MainPageView(FormView):
     success_url = '.'
 
     # add items to the context
-    def get_context_data(self,  **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         form = super().get_form()
 
         start_date, end_date = parse_daterange(form["daterange"].value())
 
-        # make plot
-        plot_div = GDeltHeatmapView.make_plot(start_date, end_date)
-        context['heatmap'] = plot_div
+        # make heatmap
+        context['heatmap'] = GDeltHeatmapView.make_heatmap(start_date, end_date)
+
+        # make radar
+        context['radar'] = GDeltHeatmapView.make_radar_chart()
+
+        # make scatter
+        context['scatter'] = GDeltHeatmapView.make_scatter_plot()
 
         # make corr matrix
         cor_div = CorrelationView.make_table(start_date, end_date)
@@ -206,6 +266,7 @@ class MainPageView(FormView):
 
     def form_valid(self, form):
         return super().get(form)
+
 
 main_page = MainPageView.as_view()
 poll_json_view = PollJSONView.as_view()
